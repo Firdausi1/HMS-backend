4d2e5d6a94443dcb35e22adaf5d2d0db8e39e89d
Firdausi1 <firdausib18@gmail.com>
2025-01-08 12:01:19 +0100

    app.js added

---
+++ app.js
@@ -0,0 +1,36 @@
+require("dotenv").config();
+
+const express = require("express");
+const MongoStore = require("connect-mongo");
+const session = require("express-session");
+const patientRoute = require("./routers/patient.route");
+const mongoose = require("mongoose");
+
+const app = express();
+
+mongoose
+  .connect(process.env.DATABASE_URI)
+  .then(() => {
+    console.log("connected to database");
+    app.listen(3001, () => {
+      console.log("Running on port 3001");
+    });
+  })
+  .catch((err) => console.log(err));
+
+//middlewares
+app.use(
+  session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: true,
+    store: MongoStore.create({
+      mongoUrl: process.env.DATABASE_URI,
+    }),
+    cookie: { maxAge: new Date(Date.now() + 3600000) },
+  })
+);
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use("/api/users", patientRoute);
