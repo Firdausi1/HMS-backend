3ee3553ab0516aa3c27c16d2dd49e00cc4a91e0c
Firdausi1 <firdausib18@gmail.com>
2025-01-08 13:05:30 +0100

    update

--- controllers/patient.controller.js
+++ controllers/patient.controller.js
@@ -1,1 +1,16 @@
 const Patient = require("../models/patient.model");
+
+const getPatient = async (req, res) => {
+  try {
+    const { patient_id } = req.query;
+    const patient = await Patient.findById(patient_id);
+    if (!patient) {
+      return res.status(401).json({ message: "Invalid patient id" });
+    }
+    res.status(200).json(patient);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+module.exports = { getPatient };
--- routers/patient.route.js
+++ routers/patient.route.js
@@ -1,1 +1,7 @@
 const express = require("express");
+const { getPatient } = require("../controllers/patient.controller");
+const router = express.Router();
+
+router.get("/patients/:patient_id", getPatient);
+
+module.exports = router;
